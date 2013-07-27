9c090e480f9f75e01912608a6fb4d8ae6648a039
Christopher <flare561@gmail.com>
2013-07-27 04:02:28 -0500

    Added many features

--- body.py
+++ body.py
@@ -1,4 +1,4 @@
-import HTMLParser,datetime,re,urllib,json,random,requests;from ircutils import format
+import HTMLParser,re,urllib,json,random,requests,datetime;from ircutils import format
 import xml.etree.ElementTree as ET
  
 #Invite Responder
@@ -92,13 +92,13 @@ if event.command in ['PRIVMSG']:
             ).encode('utf-8','replace')
         )
 
-    ##testing command, part channel
-    #if event.command.lower() in ["~part", "!part"]:
-    #    self.part_channel("#MLAS1", "I'm taking my ball and going home.")
+    #testing command, part channel
+    if event.command.lower() in ["~part", "!part"]:
+        self.part_channel("#MLAS1", "I'm taking my ball and going home.")
 
-    ##testing command, join channel
-    #if event.command.lower() in ["~join", "!join"]:
-    #    self.join_channel("#MLAS1")
+    #testing command, join channel
+    if event.command.lower() in ["~join", "!join"]:
+        self.join_channel("#MLAS1")
 
     #Random e621
     if event.command.lower() in ["~rande621", "!rande621"]:
@@ -164,7 +164,15 @@ if event.command in ['PRIVMSG']:
             randjur="Usage: ~randjur <number> Used to post random imgur pictures, <number> defines the number of results with a max of 10",
             feels="Usage: ~feels Just in case",
             help="Usage: ~help <command> The fuck do you think it does?",
-            wolf="Usage: ~wolf <query> searches wolfram alpha for your query"
+            wolf="Usage: ~wolf <query> Searches wolfram alpha for your query",
+            test="Usage: ~test Used to verify the bot is responding to messages",
+            gr="Usage: ~gr <query> links to the google results for a given query",
+            roll="Usage: ~roll <x>d<n> rolls a number, x, of n sided dice, example: 3d20",
+            ud="Usage: ~ud <query> Used to search Urban Dictionary for the first definition of a word",
+            pony="Usage: ~pony Used to show the time remaining until the next episode of MLP airs",
+            isup="Usage: ~isup <site> used to check if a given website is up using isup.me",
+            gimg="Usage: ~gimg <terms> Used to search google images with the given terms",
+            rs="Usage: ~rs <terms> Used to search for results on reddit, it's simply a google search with the restriction site:reddit.com"
             )
         if len(event.params) <= 0:
             self.send_message(
@@ -185,7 +193,7 @@ if event.command in ['PRIVMSG']:
             )
 
     #Wolfram Alpha
-    if event.command.lower() in ["!wolf", "~wolf"]:
+    if event.command.lower() in ["~wolf", "!wolf"]:
         try:
             s=requests.get("http://api.wolframalpha.com/v2/query", 
                 params=dict(
@@ -217,3 +225,172 @@ if event.command in ['PRIVMSG']:
                 "Error with the service"
             )
             raise
+
+    #Test
+    if event.command.lower() in ["~test", "!test"]:
+        possibleAnswers=[
+            "Cake, and grief counseling, will be available at the conclusion of the test.",
+            "Remember, the Aperture Science Bring Your Daughter to Work Day is the perfect time to have her tested.",
+            "As part of an optional test protocol, we are pleased to present an amusing fact: The device is now more valuable than the organs and combined incomes of everyone in *subject hometown here.*",
+            "The Enrichment Center promises to always provide a safe testing environment. In dangerous testing environments, the Enrichment Center promises to always provide useful advice. For instance: the floor here will kill you. Try to avoid it.",
+            "Due to mandatory scheduled maintenance, the next test is currently unavailable. It has been replaced with a live-fire course designed for military androids. The Enrichment Center apologizes and wishes you the best of luck. ",
+            "Well done. Here are the test results: You are a horrible person. I'm serious, that's what it says: \"A horrible person.\" We weren't even testing for that."
+            ]
+        self.send_message(
+            event.respond,
+            random.choice(possibleAnswers)
+            )
+
+    #Google Results
+    if event.command.lower() in ["~gr", "!gr:"]:
+        self.send_message(
+            event.respond,
+            "http://google.com/#q={}".format(urllib.quote(event.params, ''))
+            )
+
+    #Dice roll
+    if event.command.lower() in ["~roll", "!roll"]:
+        strippedparams=''.join(event.params.split())
+        args=strippedparams.split('d')
+
+        try:
+            numDice=int(args[0])
+        except:
+            self.send_message(
+            event.respond,
+            "Invalid parameters, expected format is <int>d<int> Example: 1d6"
+            )
+            raise
+
+        try:
+            dieSize=int(args[1])
+        except:
+            self.send_message(
+            event.respond,
+            "Invalid parameters, expected format is <int>d<int> Example: 1d6"
+            )
+            raise
+
+        if numDice > 10 or dieSize > 100:
+            self.send_message(
+            event.respond,
+            "The Maximum number of dice is 10, and the maximum number of sides is 100"
+            )
+        else:
+            self.send_message(
+            event.respond,
+            "Results: {}".format(', '.join([str(random.randint(1,dieSize)) for x in range(numDice)]))
+            )
+
+    #Urban Dictionary search
+    if event.command.lower() in ["~ud", "!ud"]:
+        try:
+            j=requests.get("http://api.urbandictionary.com/v0/define",
+                params=dict(
+                    term=event.params
+                )).json()[u'list']
+            if (len(j) > 0):
+                self.send_message(
+                    event.respond,
+                    j[0][u'definition'].encode('UTF-8', 'replace')
+                    )
+            else:
+                self.send_message(event.respond, "No Results")
+        except:
+            self.send_message(event.respond, "An error occurred while fetching your post.")
+            raise
+
+    #Time until next pony episode
+    if event.command.lower() in ["~pony", "!pony"]:
+        class LocalTZ(datetime.tzinfo):
+            _unixEpochOrdinal = datetime.datetime.utcfromtimestamp(0).toordinal()
+
+            def dst(self, dt):
+                return datetime.timedelta(0)
+
+            def utcoffset(self, dt):
+                t = (dt.toordinal() - self._unixEpochOrdinal)*86400 + dt.hour*3600 + dt.minute*60 + dt.second + time.timezone
+                utc = datetime.datetime(*time.gmtime(t)[:6])
+                local = datetime.datetime(*time.localtime(t)[:6])
+                return local - utc
+
+        class UTC(tzinfo):
+            """UTC"""
+        
+            def utcoffset(self, dt):
+                return timedelta(0)
+        
+            def tzname(self, dt):
+                return "UTC"
+        
+            def dst(self, dt):
+                return timedelta(0)
+
+        if datetime.datetime.now(LocalTZ()) < datetime.datetime(year=2013, month=11, day=23, hour=15, minute=30, tzinfo=UTC()):
+            wait=datetime.datetime(year=2013, month=11, day=23, hour=15, minute=30, tzinfo=UTC()) - datetime.datetime.now(LocalTZ())
+        else:
+            airdate=datetime.datetime.now(LocalTZ()) + timedelta((12 - datetime.datetime.now(LocalTZ()).weekday()) % 7)
+            wait = datetime.datetime(year=airdate.year, month=airdate.month, day=airdate.day, hour=15, minute=30, tzinfo=UTC()) - datetime.datetime.now(LocalTZ())
+        self.send_message(
+            event.respond,
+            '{} Days, {} Hours, {} Minutes remaining until the next episode'.format(
+                wait.days,
+                wait.seconds/3600,
+                wait.seconds/60%60
+                )
+            )
+
+    #isup.me check if website is up
+    if event.command.lower() in ["~isup", "!isup"]:
+        try:
+            s=requests.get("http://isup.me/{}".format(
+                event.params
+                )).text
+            if "It's just you." in s:
+                response="It's just you! {} is up!".format(event.params)
+            else:
+                response="It's not just you! {} looks down from here!".format(event.params)
+            self.send_message(
+                event.respond,
+                response
+            )
+        except:
+            self.send_message(
+                event.respond,
+                "Error with the service"
+            )
+            raise
+
+    #Google Image Search
+    if event.command.lower() in ['~gimg','!gimg']:
+        j=requests.get(
+            'https://ajax.googleapis.com/ajax/services/search/images',
+            params=dict(
+                v="1.0",
+                q=event.params
+            )
+        ).json()[u'responseData'][u'results'][0]
+        self.send_message(
+            event.respond,
+            u'{}: {}'.format(
+                HTMLParser.HTMLParser().unescape(j[u'titleNoFormatting']),
+                j[u'unescapedUrl']
+            ).encode('utf-8','replace')
+        )
+
+    #Reddit Search
+    if event.command.lower() in ['~rs','!rs']:
+        j=requests.get(
+            'https://ajax.googleapis.com/ajax/services/search/web',
+            params=dict(
+                v="1.0",
+                q=event.params + " site:reddit.com"
+            )
+        ).json()[u'responseData'][u'results'][0]
+        self.send_message(
+            event.respond,
+            u'{}: {}'.format(
+                HTMLParser.HTMLParser().unescape(j[u'titleNoFormatting']),
+                j[u'unescapedUrl']
+            ).encode('utf-8','replace')
+        )
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import sys;import traceback;from ircutils import bot
+from datetime import tzinfo,timedelta
+import time,datetime
+
+
 class q(bot.SimpleBot):
   def on_any(self,event):
     try:
@@ -8,7 +12,8 @@ class q(bot.SimpleBot):
     except:
       print "ERROR",str(sys.exc_info())
       print traceback.print_tb(sys.exc_info()[2])
- 
+
 if __name__ == "__main__":
   s=q("LyraBot");s.connect("irc.mlas1.com", channel="#bottest")
   s.start()
+
