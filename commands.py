8a71715520a12966a0f3704e94dd9d3b55ad84bd
flare561 <flare561@gmail.com>
2015-06-15 23:06:44 -0500

    Forgot to enable ssl checks when moving to live code

--- commands.py
+++ commands.py
@@ -597,8 +597,8 @@ class commands:
         body=dict()
         body['authenticity_token']=authenticitytoken
         body['_method']='patch'
-        sess.post('https://derpiboo.ru/filters/select?id=6758f0d16361640e71480000', verify=False, data=body, headers=dict(Referer='https://derpiboo.ru/filters'))
-        results=sess.get('https://derpiboo.ru/search.json', verify=False, data=dict(q=event.params)).json()['search']
+        sess.post('https://derpiboo.ru/filters/select?id=6758f0d16361640e71480000', data=body, headers=dict(Referer='https://derpiboo.ru/filters'))
+        results=sess.get('https://derpiboo.ru/search.json', data=dict(q=event.params)).json()['search']
         if len(results) > 0:
             choice=random.choice(results)
             idNum=choice['id_number']
