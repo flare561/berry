f8f3575b31c4c757c5df4a7cc2ff20ec46f530f5
mrhoontar <mrhoontar@gmail.com>
2017-05-19 20:58:55 -0400

    fixed command_fwt
    now it works for lowercase letters and symbols

--- commands.py
+++ commands.py
@@ -961,7 +961,7 @@ class commands:
     def command_fwt(self, event):
         '''Usage: ~fwt <text> repeats text in full width and adds spaces for A E S T H E T I C S'''
         self.send_action(event.respond, (u" ".join([
-            unichr(ord(x) + 65248) if 64 < ord(x) < 91 else x
+            unichr(ord(x) + 65248) if 32 < ord(x) < 127 else x
             for x in event.params.upper()
         ])).encode('utf-8', 'replace'))
 
